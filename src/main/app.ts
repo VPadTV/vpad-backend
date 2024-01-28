0a68734bbe32c25122375dc87f80ad7f977a623c
Jaquie <jaq.cat@protonmail.ch>
2024-01-28 12:01:50 -0300

    rayte limit

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,6 +1,14 @@
 import express from 'express'
 import routes from './routes';
 import bodyParser from 'body-parser';
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+})
 
 export class App {
     public server: express.Application
@@ -9,6 +17,7 @@ export class App {
         this.server = express()
         // this.server.use(express.json({ limit: '10mb' }))
         this.server.use(bodyParser.urlencoded({ extended: true }));
+        this.server.use(limiter)
 
         for (let path in routes) {
             const router = express.Router()
